4967e19a563643a891018031a0cdac5dce887d53
dtl915 <tianle.harry.duan@gmail.com>
2026-09-15 14:31:08 -0400

    fixed bugs in multiples, added overload for multiples, added reduce file

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -6,9 +6,23 @@ public class Multiples {
         System.out.println(cnt);
     }
 
+
+    static int multiples() {
+        int cnt = 0;
+        for (int i=1;i<1000;i++)
+        {
+            if(i%3==0 || i%5==0)
+            {
+                cnt++;
+            }
+
+        }
+        return cnt;
+    }
+
     static int multiples(int n, int a, int b) {
         int cnt = 0;
-        for (int i=0;i<n;i++)
+        for (int i=1;i<n;i++)
         {
             if(i%a==0 || i%b==0)
             {
